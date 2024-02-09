7e3bc51c878483e2d96b1ab81a96b64c0b33e432
Ibrahim Aderinto <43772814+Tunde4real@users.noreply.github.com>
2024-02-09 16:05:00 +0100

    Update OfferUpScrape.py

--- OfferUpScrape.py
+++ OfferUpScrape.py
@@ -1,181 +1,288 @@
-# -- coding: utf-8 -
-# Written by Ibrahim Aderinto; contact @ ib4real914@gmail.com
+# -*- coding: utf-8 -*-
+# written by Ibrahim Aderinto
 
+# import system module
+import re
+import sys
+import time
+import json
+import random
+from datetime import datetime
+
+# import third party modules
+import gspread
+import pandas as pd
+from IPython.display import clear_output
+from extension import proxies
+from df2gspread import df2gspread as d2g
+from oauth2client.service_account import ServiceAccountCredentials
 from selenium import webdriver
-import psycopg2, time, datetime, csv, pandas as pd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def chrome():
-    ''' Fucntion for browsing with chrome '''
+""" Module Documentation: Scrapes products from OfferUp
+"""
 
-    from selenium.webdriver.chrome.options import Options
-    #       Headless mode
-    # chrome_option = Options()
-    # chrome_option.add_argument("--headless")
-    # driver = webdriver.Chrome(options=chrome_option)
+def chrome(mode='h') -> object:
+    '''A function to instantiate chrome driver
 
-    # Browser mode
-    driver = webdriver.Chrome()
+        :arguments: 
+            mode: representing either headless (preferred) or browser mode.
 
-    return driver
+        :returns: 
+            driver: the driver instantiated.
+    '''
+    username = 'deleted'
+    password = 'deleted'
+    endpoint = 'gate.smartproxy.com'
+    port = '7000'
+    chrome_options = Options()
+    # bypass OS security
+    chrome_options.add_argument('--no-sandbox')
+    # overcome limited resources
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    # disable chrome notifications
+    chrome_options.add_argument("--disable-notifications")
+    # don't tell chrome that it is automated
+    chrome_options.add_experimental_option(
+        "excludeSwitches", ["enable-automation"])
 
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36")
+    chrome_options.add_argument("disable-infobars")
+    # set download path
+    prefs = {
+        "credentials_enable_service": False,
+        "profile.password_manager_enabled": False,
+        "download.prompt_for_download": False,
+        "plugins.always_open_pdf_externally": True,
+        "download.directory_upgrade": True,
+        "useAutomationExtension": False
+    }
+    chrome_options.add_experimental_option("prefs", prefs)
+    proxies_extension = proxies(username, password, endpoint, port)
 
-def firefox():
-    ''' Fucntion for browsing with firefox '''
-
-    from selenium.webdriver.firefox.options import Options
-    #       Headless mode
-    # firefox_options  = Options()
-    # firefox_options.add_argument('-headless')
-    # driver = webdriver.Firefox(options=firefox_options,
-    #                               executable_path=r'C:\Users\ib4re\Desktop\Desktop Folders\Orlando\geckodriver.exe')
-
-    # Browser mode
-    try:
-        driver = webdriver.Firefox()
-    except:
-        print('webdriver for firefox not installed')
-
-    return driver
-
-
-def edge():
-    ''' Fucntion for browsing with Microsoft edge '''
-
-    from selenium.webdriver.edge.options import Options
-    #       Headless mode
-    # edge_options = Options()
-    # edge_options.add_argument('--headless')
-    # driver = webdriver.Edge(options=edge_options, 
-    #                           executable_path=r'C:\Users\ib4re\Desktop\Desktop Folders\Orlando\edgedriver_win64\msedgedriver.exe')
-
-    # Browser mode
-    try:
-        driver = webdriver.Edge()
-    except:
-        print("webdriver for Microsoft Edge not installed")
-
-    return driver
-
-
-def tear(address, browser):
-    # Defining output lists
-    names, prices, locations, imgs, urls= [], [], [], [], []
-
-    browser = browser.lower()
-    # Accessing the page
-    if browser == 'chrome':
-        driver = chrome()
-    elif browser == 'firefox':
-        driver = firefox()
-    elif browser == 'edge':
-        driver == edge()
-
-    driver.get(address)
-    time.sleep(20) # The program sleeps for 20 secs to allow all search results load
-
-    lenght_div = len(list(driver.find_elements_by_xpath("//div[@id='db-item-list']/div")))
+    chrome_options.add_extension(proxies_extension)
     
-    x = 1
-    while lenght_div >= x:
-        lenght_a = len(list(driver.find_elements_by_xpath("//div[@id='db-item-list']/div[{}]/a".format(x))))
-
-        y = 1
-        while lenght_a >= y:
-            img_tag = driver.find_element_by_xpath("//div[@id='db-item-list']/div[{}]/a[{}]/div/div/div/img".format(x, y))
-            img_src = img_tag.get_attribute("data-src")
-            
-            name_tag = driver.find_element_by_xpath("//div[@id='db-item-list']/div[{}]/a[{}]/div/div[2]/span".format(x, y))
-            name = name_tag.text
-
-            loc_tag = driver.find_element_by_xpath("//div[@id='db-item-list']/div[{}]/a[{}]/div/div[2]/span[2]".format(x, y))
-            loc = loc_tag.text
-
-            price_tag = driver.find_element_by_xpath("//div[@id='db-item-list']/div[{}]/a[{}]/div/div[2]/div/span".format(x, y))
-            price = price_tag.text
-
-            a_tag = driver.find_element_by_xpath("//div[@id='db-item-list']/div[{}]/a[{}]".format(x, y))
-            url = a_tag.get_attribute("href")
-
-            prices.append(price)
-            names.append(name)
-            locations.append(loc)
-            imgs.append(img_src)
-            urls.append(url)
-
-            y += 1
-        
-        x += 1
-
-    driver.close()
-    return names, prices, locations, imgs, urls
-
-
-def write_database(names, prices, locations, imgs, urls, user, instance):
-    timestamp = str(datetime.datetime.today())
+    # instantiate a non-headless browser
+    if mode=='b':
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        driver.maximize_window()
+        return driver
     
-    try:
-        connection = psycopg2.connect(user = "godwiclg",
-                                    password = "MbsfkOnYP8O6Su4hr3Kf6OS9rpKyVN6t",
-                                    host = "ruby.db.elephantsql.com",
-                                    port = "5432",
-                                    database = "godwiclg")
-
-        cursor = connection.cursor()
-
-        data_url_query = '''SELECT url FROM Results'''
-        cursor.execute(data_url_query)
-        data_url = cursor.fetchall()
-
-        db_urls = [urldb[0] for urldb in data_url]
-
-        x = 0
-        for url in urls:
-            if url in db_urls:
-                update_query = '''UPDATE INTO Results WHERE url = '%s' SET price = '%s', 
-                                    description = '%s', location = '%s', image_Url = '%s',
-                                    time_last_scraped = '%s', user_instance_time_last_scraped = '%s'
-                                    '''%(url, prices[x], names[x], locations[x], imgs[x],
-                                    timestamp, user+' '+instance+' '+timestamp)
-                cursor.execute(update_query)
-                connection.commit()
-            else:
-                update_query = '''INSERT INTO Results VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s') 
-                                    '''%(names[x], prices[x], locations[x], urls[x], imgs[x], timestamp, timestamp, user+' '+instance+' '+timestamp)
-                cursor.execute(update_query)
-                connection.commit()
-                print(names[x], prices[x], locations[x], urls[x], imgs[x], user+' '+instance+' '+timestamp, timestamp)
-            x += 1
-
-    except (Exception, psycopg2.Error) as error :
-        print ("Error while connecting to PostgreSQL", error)
-    finally:
-        #closing database connection.
-            if(connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed")
+    # instantiate a headless browser
+    chrome_options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.set_window_size(1920,1080)
+    return driver
 
 
-def main():
-    # Reading inputs from sample input.csv
-    input_df = pd.read_csv('sample input.csv')
-    user = str(list(input_df['User']))[2:-2]
-    instance = str(list(input_df['Instance']))[1:-1]
-    interval = str(list(input_df['Interval']))[1:-1]
-    browser = str(list(input_df['Browser']))[2:-2]
-    url = str(list(input_df['Address']))[2:-2]
+def get_credentials():
+    '''
+        A fucntion that returns credentials object that can be used to authenticate a
+        google client. A google client is an object that has access to certain resources
+        of the account who owns the credentials.
 
-    results = tear(url, browser)
-    names, prices, locations, imgs, urls = results[0], results[1], results[2], results[3], results[4]
-    write_database(names, prices, locations, imgs, urls, user, instance)
+        arguments:
+            None
 
-    return interval
+        returns:
+            credentials -  a credentials object.
+    '''
+    scope = [\
+            "https://spreadsheets.google.com/feeds",
+            'https://www.googleapis.com/auth/spreadsheets',
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive"
+    ]
 
-if __name__ == "__main__":
-    z = 1
-    while True:
-        interval = main()
-        print("Done with scrape {} \n".format(z))
-        time.sleep(int(str(interval)))
-        z+=1
+    key_file = {
+        "type": "deleted",
+        "project_id": "deleted",
+        "private_key_id": "deleted",
+        "private_key": "deleted",
+        "client_email": "deleted",
+        "client_id": "deleted",
+        "auth_uri": "deleted",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": "deleted"
+    }
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_dict(key_file, scopes=scope)
+    return credentials
+
+
+def get_parameters(google_client):
+    '''
+        A function to access the google spread sheet storing the parameters needed
+        to run the bot and return them. The parameters are search keyword, limit
+        (of products to be scraped), search radius, sort_option and zip codes.
+
+        arguments:
+            google_client - an authenticated google client.
+
+        returns:
+            parameters - a dictionary storing all parameters other than the zip codes.
+            zip_codes - a list of all zip codes.
+    '''
+    # open specification sheet and get needed values
+    spec_sheet = google_client.open("Specifications").sheet1
+    spec_sheet = spec_sheet.get_all_values()
+    limit, keyword, radius, sort_option = spec_sheet[1]
+    limit = int(limit)
+
+    # open zip code sheets and get needed values
+    zip_sheet = google_client.open("Zip Codes").sheet1
+    zip_sheet = zip_sheet.get_all_values()
+    zip_codes = [i[-1] for i in zip_sheet[1:]]
+
+    return {'limit':limit, 'search_keyword':keyword, 'search_radius':radius, 'sort_option':sort_option}, zip_codes
+
+
+def parse_url(parameters):
+    '''
+        A method to contruct the full api url to access offerup
+
+        arguments:
+            parameters - a dict object that includes the parameters to be added to the
+                         base_url. It includes values for limit, zipcode, page, search_keyword
+                         and search_radius.
+
+        returns:
+            base_url - a string of the parsed url
+    '''
+    base_url = 'https://offerup.com/api/search/v4/feed?'
+    base_url += f"limit={parameters['limit']}" + f"&q={parameters['search_keyword']}"
+    base_url += f"&zipcode={parameters['zip_code']}" + f"&radius={parameters['search_radius']}"
+    sort_option = ''
+    if 'default(nearest first)' == parameters['sort_option']:
+        return base_url
+    elif 'price: low to high' == parameters['sort_option']:
+        sort_option = 'price'
+    elif 'price: high to low' == parameters['sort_option']:
+        sort_option = '-price'
+    base_url += f"&sort={sort_option}"
+    return base_url
+
+
+def transform(data):
+    '''
+        A function that transforms the search result data on the page into a suitable format
+
+        arguments:
+            data - a string of all the text on the page
+
+        returns:
+            items - a list of lists where each member list represents the
+                    details of a product.
+    '''
+    data = json.loads(data)
+    product_items = data['data']['feed_items']
+    products = []
+    for item in product_items:
+        try:
+            item = item['item']
+            if bool(item['paid'])==True:
+                continue
+            date_posted = item['post_date']
+            date_posted = date_posted.replace('T', ' ')
+            date_posted = date_posted.replace('Z', '')
+            image_link = item['photos'][0]['images']['detail']['url']
+            image = f"=IMAGE(\"{image_link}\", 4, 60, 60)"
+            title = item['title']
+            price = float(item['price'])
+            location = item['location_name']
+            product_link = item['get_full_url']
+            products += [[image, title, price, location, image_link, product_link, date_posted]]
+        except:
+            continue
+    return products
+
+
+def load_data(google_client, df):
+    '''
+        A method to write data into online google sheet.
+
+        arguments:
+            google_client - an authenticated google client.
+            df - a dataframe containing the data to be written into the online sheet.
+
+        returns:
+            None
+    '''
+
+    spreadsheet_key = '12XIAXMmNNKsZExRkdAOEKbsXDX5Rl2rZNvQvHv7WgWM'
+    wks_name = 'Sheet1' # worksheet name just in case you have multiple worksheets
+    credentials = get_credentials()
+
+    # upload df to spreadsheet, that is save data scrapped to spreadsheet.
+    d2g.upload(df, spreadsheet_key, wks_name, credentials=credentials, row_names=False)
+    time.sleep(5)
+
+    # To solve the problem of incorrectly formatted image links formula
+    sheet = google_client.open("OfferUpScrape").sheet1
+    data = sheet.get_all_records()  # Get all data as a list of dictionaries, where each dictionary represents a row
+    l = len(data)
+    cell_list = sheet.range(f'A1:A{l}')
+    sheet.update_cells(cell_list, value_input_option='USER_ENTERED')    # update all rows of first cell
+    sheet.update('A1', 'Image', value_input_option='USER_ENTERED')  #update cell title
+    print('Successfully uploaded data to Google Sheet')
+
+
+def main(driver):
+    '''
+        The main function
+
+        arguments:
+            driver - an instantiated chrome webdriver object
+
+        returns:
+            None
+    '''
+    credentials = get_credentials()
+    google_client = gspread.authorize(credentials)
+    parameters, zip_codes = get_parameters(google_client)
+    random.shuffle(zip_codes)
+
+    all_products = []
+    for code in zip_codes:
+        # print(f'Scraping for zipcode {code}')
+        parameters['zip_code'] = code
+        url = parse_url(parameters)
+
+        wait_time = 0
+        while True:
+          driver.get(url)
+          time.sleep(wait_time)
+          page_data = driver.find_element(By.TAG_NAME, 'body').text
+          if page_data.strip() != '' or wait_time==20:
+              break
+          wait_time += 2
+        time.sleep(10)
+        print(page_data)
+
+        products = transform(page_data)
+        all_products += products
+        time.sleep(2)
+
+    df = pd.DataFrame(all_products, columns=['Image', 'Title', 'Price', 'Location', 'Image Link', 'Product Link', 'Date Posted'])
+    df['Date Posted'] = pd.to_datetime(df['Date Posted'])
+
+    # sort the data
+    if parameters['sort_option'] == 'default(nearest first)':
+        df = df.sort_values(by='Date Posted', ascending=False)
+    elif parameters['sort_option'] == 'price: high to low':
+        df = df.sort_values(by='Price', ascending=False)
+    elif parameters['sort_option'] == 'price: low to high':
+        df = df.sort_values(by='Price', ascending=True)
+
+    df = df.drop_duplicates()
+    load_data(google_client, df)
+
+
+if __name__=="__main__":
+    driver = chrome('b')
+    main(driver)
